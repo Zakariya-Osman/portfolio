dedc5d6132022912348e7b5df4b611b65d551a1d
Zackaria Osman <cobalt.zr86@gmail.com>
2025-01-06 19:41:52 -0700

    Got it wokring

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 // path: src/index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals.mjs";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -14,4 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
